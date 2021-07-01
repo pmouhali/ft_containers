3261122df5a823ba24611cdc00a66af27dfae416
pmouhali <p.m.272019@gmail.com>
2021-07-01 11:18:59 +0200

    reprise pour update sujet

---
+++ bst.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "../bst.hpp"
+
+int 	main(void) {
+
+	ft::bst<int> tree;
+
+	tree._root = new ft::bst<int>::node(8, NULL, NULL, NULL);
+	/* left side assign */
+	tree._root->_left = new ft::bst<int>::node(3, tree._root, NULL, NULL);
+	tree._root->_left->_left = new ft::bst<int>::node(1, tree._root->_left, NULL, NULL);
+	tree._root->_left->_right = new ft::bst<int>::node(6, tree._root->_left, NULL, NULL);
+
+	tree._root->_left->_right->_left = new ft::bst<int>::node(4, tree._root->_left->_right, NULL, NULL);
+	tree._root->_left->_right->_right = new ft::bst<int>::node(7, tree._root->_left->_right, NULL, NULL);
+
+	/* right side assign */
+	tree._root->_right = new ft::bst<int>::node(10, tree._root, NULL, NULL);
+	tree._root->_right->_right = new ft::bst<int>::node(14, tree._root->_right->_right, NULL, NULL);
+
+	tree._root->_right->_right->_left = new ft::bst<int>::node(13, tree._root->_left->_right, NULL, NULL);
+
+	/* mock the leftmost node value */
+	tree._leftmost = tree._root->_left->_left;
+
+	/* expectations print */
+	std::cout << "expect: 1 3 4 6 7 8 10 13 14\n   got: ";
+
+	/* setup iterator */
+	ft::bst<int>::iterator	it = tree.begin();
+	ft::bst<int>::iterator	it_end = tree.end();
+/*
+	while (it != it_end) {
+		std::cout << it._n->_data << " ";
+		it++;
+	}
+*/
+
+
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+	it++;
+	std::cout << it._n->_data << " ";
+
+	std::cout << std::endl;
+}
---
+++ bst.hpp
@@ -0,0 +1,150 @@
+/* BINARY SEARCH TREE */
+
+// TODO: goto_next_node isnt functional, solve the issue. See test results at
+// tests/bst.cpp
+
+#ifndef BST_HPP
+# define BST_HPP
+
+# include "shared.hpp"
+
+namespace ft {
+
+	template < class T >
+	class bst {
+
+//		private:
+		public: // TODO: enlever
+
+		class node;
+
+		class node {
+
+			public:
+
+			node (const T & data, node* parent, node* left, node* right)
+				: _data(data), _parent(parent), _left(left), _right(right) {}
+	
+			~node () {}
+
+			T		_data;
+			node*	_parent;
+			node*	_left;
+			node*	_right;
+		};
+
+/* bst::attributes  */
+
+		node* _root;
+		node* _leftmost;
+		node* _rightmost;
+
+/* bst::private_methods */
+
+		void	_clear(node* n) {
+			if (n) {
+				_clear(n->_right);
+				_clear(n->_left);
+				delete n;
+			}
+		}
+
+//		public: TODO: décommenter
+
+		class iterator;
+
+		class iterator {
+
+			/*
+			**	[NAME] : goto_next_node
+			**
+			**	[DESCRIPTION] :
+			**
+			**	Returns a pointer to the given node's in order successor.
+			**	Encapsulate the logic behind binary search tree iteration.
+			*/
+
+			node	*goto_next_node (node *c) {
+				node	*p;
+
+				if (c == NULL)
+					return (NULL);
+
+				if (c->_right != NULL) {
+					c = c->_right;
+					while (c->_left != NULL)
+						c = c->_left;
+				}
+				else {
+					p = c->_parent;
+					while (p != NULL && c == p->_right) {
+						c = p;
+						p = p->_parent;
+					}
+					c = p;
+				}
+
+				return c;
+			}
+
+			public:
+
+			node	*_n;
+
+			iterator () : _n(NULL) {}
+
+			iterator (node *n) : _n(n) {}
+
+			iterator (const iterator & x) : _n(x._n) {}
+
+			iterator&	operator=(const iterator & x) {
+				_n = x._n;
+				return *this;
+			}
+
+			~iterator () {}
+
+			iterator	operator++ () {
+				_n = goto_next_node(_n);
+				return *this;
+			}
+
+			iterator	operator++ (int) {
+				iterator tmp = *this;
+				++*this;
+				return tmp;
+			}
+
+			bool		operator!= (const iterator & x) {
+				return (_n != x._n);
+			}
+
+		};
+
+/* bst::default_constructor */
+
+		bst () : _root(NULL), _leftmost(NULL), _rightmost(NULL) {}
+
+/* bst::destructor */
+		~bst () {
+			clear();
+		}
+
+/* bst::begin */
+		iterator begin () {
+			return _leftmost;
+		}
+
+/* bst::clear */
+		void	clear () {
+			_clear(_root);	
+		}
+
+/* bst::end */
+		iterator end() {
+			return iterator();
+		}
+	};
+}
+
+#endif
